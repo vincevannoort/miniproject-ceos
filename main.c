d217b2404f7f738a675547dd44999172115f56c8
Vince van Noort <vince.vannoort@student.hu.nl>
2017-03-15 10:21:29 +0100

    Documentatie

---
+++ main.c
@@ -0,0 +1,15 @@
+/* --------------------------- */
+// Vince van Noort & Quentin Hoogwerf
+// Miniproject CEOS
+/* --------------------------- */
+#include <stdio.h>
+
+int main(void) {
+    // 1. Er wordt game gestart
+    // 2. Random tijd wachten voordat de game echt start
+    // 3. In tussentijd kijken voor valse start
+    // 4. Game starten en timer beginnnen (lichtjes of buzzer aanzetten)
+    // 5. Wachten op user input
+    // 6. Gemeten tijd & wie gewonnen heeft tonen
+    return 0;
+}
