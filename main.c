bc51b0b871bcd657aed9474358f040a0d2869872
Vince van Noort <vince.vannoort@student.hu.nl>
2017-03-15 11:59:07 +0100

    Make loop, and add highscore txt

--- main.c
+++ main.c
@@ -20,35 +20,39 @@
 #define ANSI_COLOR_RESET   "\033[1m\x1b[0m"
 
 int main(void) {
-    // 1. Er wordt game gestart
-    setupPins();
+    for (int i = 0; i < 10; ++i) {
 
-    // 2. Random tijd wachten voordat de game echt start
-    srand(time(NULL));
-    int random = rand() % 500 + 1;
-    printf("TIJDELIJK: wachten voor %d milliseconden.\n", random * 10);
+        // 1. Er wordt game gestart
+        setupPins();
 
-    // 3. In tussentijd kijken voor valse start
-    int falseStart = checkForButtonInput(random, false);
-    if (falseStart) {
-        printf("Valse start: %d \n", checkForButtonInput(random, false));
-        return 0;
+        // 2. Random tijd wachten voordat de game echt start
+        srand(time(NULL));
+        int random = rand() % 500 + 1;
+        printf("TIJDELIJK: wachten voor %d milliseconden.\n", random * 10);
+
+        // 3. In tussentijd kijken voor valse start
+        int falseStart = checkForButtonInput(random, false);
+        if (falseStart) {
+            printf("Valse start: %d \n", checkForButtonInput(random, false));
+            return 0;
+        }
+
+        // 4. Game starten en timer beginnnen (lichtjes of buzzer aanzetten)
+        struct timeval start, stop;
+        double secs = 0;
+
+        gettimeofday(&start, NULL);
+        printf("Winner: " ANSI_COLOR_GREEN "player %d." ANSI_COLOR_RESET "\n", checkForButtonInput(500, true));
+        gettimeofday(&stop, NULL);
+
+        secs = (double)(stop.tv_usec - start.tv_usec) / 1000000 + (double)(stop.tv_sec - start.tv_sec);
+        printf("time taken %f\n",secs);
+
+        // 5. Save highscores
+        // FILE *highscorefile = fopen("highscore.txt", "r");
+
     }
 
-    // 4. Game starten en timer beginnnen (lichtjes of buzzer aanzetten)
-    struct timeval start, stop;
-    double secs = 0;
-
-    gettimeofday(&start, NULL);
-    printf("Winner: " ANSI_COLOR_GREEN "player %d." ANSI_COLOR_RESET "\n", checkForButtonInput(500, true));
-    gettimeofday(&stop, NULL);
-
-    secs = (double)(stop.tv_usec - start.tv_usec) / 1000000 + (double)(stop.tv_sec - start.tv_sec);
-    printf("time taken %f\n",secs);
-
-    // 5. Wachten op user input
-
-    // 6. Gemeten tijd & wie gewonnen heeft tonen
 
     return 0;
 }
