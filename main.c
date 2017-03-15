38b0cdd7a2d39e115377b572dd6048a4392bb999
Vince van Noort <vince.vannoort@student.hu.nl>
2017-03-15 11:14:13 +0100

    Update

--- main.c
+++ main.c
@@ -13,14 +13,14 @@ int main(void) {
     setupPins();
 
     // 2. Random tijd wachten voordat de game echt start
-    int random = rand() % 5;
-    printf("TIJDELIJK: wachten voor %d seconden.\n", random);
+    int random = rand() % 500;
+    printf("TIJDELIJK: wachten voor %d milliseconden.\n", random);
 
     // 3. In tussentijd kijken voor valse start
     printf("Valse start: %d \n", checkForButtonInput(random, false));
 
     // 4. Game starten en timer beginnnen (lichtjes of buzzer aanzetten)
-    printf("Winner: %d\n", checkForButtonInput(5, true));
+    printf("Winner: %d\n", checkForButtonInput(500, true));
 
     // 5. Wachten op user input
 
